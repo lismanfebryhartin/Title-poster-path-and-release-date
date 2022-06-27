6784cb93dfe508582a7f94852fa104fb14662385
lismanfebryhartin <lismanfh00@gmail.com>
2022-06-27 22:56:15 +0700

    first commit

---
+++ app/src/main/java/com/example/movie_catalogue/MovieApiInterface.kt
@@ -0,0 +1,9 @@
+package com.example.movie_catalogue
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MovieApiInterface {
+    @GET("/3/movie/popular?api_key=21aada851713c086f599c5043491bca2")
+    fun getMovieList(): Call<MovieResponse>
+}
